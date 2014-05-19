c42ad18047120825f22893ce359e13fedcb17f65
Gui Chen <gui.chen@intel.com>
2014-05-19 02:45:32 -0400

    remove --preserve-order option in tar fs image
    Change-Id: Ib510143290f49c1b791424201401085368b9d1bb
    Signed-off-by: Gui Chen <gui.chen@intel.com>

--- mic/imager/fs.py
+++ mic/imager/fs.py
@@ -78,7 +78,6 @@ class FsImageCreator(BaseImageCreator):
             tar = find_binary_path('tar')
             tar_cmdline = [tar, "--numeric-owner",
                                 "--preserve-permissions",
-                                "--preserve-order",
                                 "--one-file-system",
                                 "--directory",
                                 self._instroot]
